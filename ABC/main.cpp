5426c12ecc11830de496d7afaf402de90108c562
延原昇吾 <nobubaranoboruware@nobubaranoboruwarenoMacBook-Pro.local>
2017-04-20 18:12:15 +0900

    Initial Commit

---
+++ ABC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ABC
+//
+//  Created by 延原昇吾 on 2017/04/20.
+//  Copyright © 2017年 Shogo N. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
